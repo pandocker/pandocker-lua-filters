ae919c8ce7aa20c1d8ebcefdfcd39819f16c6c36
K4zuki <k.yamamoto.08136891@gmail.com>
2019-07-04 17:28:18 +0900

    Update docx-pagebreak-toc.lua
    not linked bug is fixed

--- lua/docx-pagebreak-toc.lua
+++ lua/docx-pagebreak-toc.lua
@@ -12,7 +12,7 @@ local raw_toc = [[
         <w:p>
             <w:r>
                 <w:fldChar w:fldCharType="begin" w:dirty="true" />
-                <w:instrText xml:space="preserve">TOC \\o "1-3" \\h \\z \\u</w:instrText>
+                <w:instrText xml:space="preserve">TOC \o "1-3" \h \z \u</w:instrText>
                 <w:fldChar w:fldCharType="separate" />
                 <w:fldChar w:fldCharType="end" />
             </w:r>
