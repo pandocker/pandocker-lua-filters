9c8915979b02ac177680265527fa62ecc5716c10
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-07-11 20:58:31 +0900

    update tex-landscape
    - just see Div and test class
    - *much* simpler implement!

--- lua/tex-landscape.lua
+++ lua/tex-landscape.lua
@@ -37,27 +37,13 @@ local function get_vars (meta)
     start_landscape = pandoc.RawBlock("latex", "\\newgeometry{" .. stringify(_meta) .. "}\\begin{landscape}")
 end
 
-function landscape(doc)
-    local head = {}
-    local tail = { stop_landscape }
-    for i, el in ipairs(doc.blocks) do
-        --print(i .. " " .. el.tag .. "(" .. stringify(el) .. ")")
-        if el.tag == "Div" and el.classes:find("LANDSCAPE") then
-            debug(MESSAGE)
-            table.move(doc.blocks, 1, i - 1, 1, head) -- head has contents before #include
-            table.insert(head, start_landscape)
-            --dump(head, "hh")
-            --dump(el.content, "ss")
-            table.move(doc.blocks, i + 1, #doc.blocks, 2, tail) -- tail has after #include
-            --dump(tail, "tt")
-            table.move(el.content, 1, #el.content, #head + 1, head) -- concat head and sub.blocks -> head
-            table.move(tail, 1, #tail, #head + 1, head) -- concat head and tail
-            --dump(head, "    ")
-            doc.blocks = head
-        end
+local function landscape(el)
+    if el.classes:find("LANDSCAPE") then
+        debug(MESSAGE)
+        table.insert(el.content, 1, start_landscape)
+        table.insert(el.content, stop_landscape)
+        return el.content
     end
-    --dump(doc.blocks, "    ")
-    return doc
 end
 
-return { { Meta = get_vars }, { Pandoc = landscape } }
+return { { Meta = get_vars }, { Div = landscape } }
