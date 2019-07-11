5112b3b4cf8e1e8eac69d46c5d8a5df908755721
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-07-11 21:57:51 +0900

    add tex-rowcolors-reset.lua; update testing Makefile
    - works with latex output only
    - adds a RawBlock before each Table
    - takes 'tex-rowcolors' metadata
        - takes default value if not found in source

---
+++ lua/tex-rowcolors-reset.lua
@@ -0,0 +1,37 @@
+--[[
+# tex-rowcolors-reset.lua
+
+Finds table and inserts tex command to reset row rule
+]]
+
+local debug = require("pandocker.utils").debug
+local stringify = require("pandoc.utils").stringify
+local default_meta = require("pandocker.default_loader")["tex-rowcolors"]
+local MESSAGE = "[ lua ] Table found"
+local meta = {}
+local NOT_FOUND = "[ lua ] metadata '%s' was not found in source, applying default %s."
+local reset_colors = {}
+
+local function dump(tt, mm)
+    for ii, vv in ipairs(tt) do
+        print(mm .. ii .. " " .. tostring(vv["tag"]) .. "(" .. stringify(vv) .. ")")
+    end
+end
+
+local function get_vars (mt)
+    meta = mt["tex-rowcolors"]
+    if meta == nil then
+        meta = default_meta
+        debug(string.format(NOT_FOUND, "tex-rowcolors", ""))
+    end
+    reset_colors = pandoc.RawBlock("latex", stringify(meta))
+end
+
+local function reset_table_color(el)
+    if FORMAT == "latex" then
+        return { reset_colors, el }
+    end
+
+end
+
+return { { Meta = get_vars }, { Table = reset_table_color } }
