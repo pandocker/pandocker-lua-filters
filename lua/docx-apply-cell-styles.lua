b3be9c074575e26f09318d5bb01bbe20db01d744
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-12-27 21:27:06 +0900

    update docx-apply-cell-styles.lua; update tests/Makefile

--- lua/docx-apply-cell-styles.lua
+++ lua/docx-apply-cell-styles.lua
@@ -15,63 +15,63 @@ local default_meta = require("pandocker.default_loader")[meta_key]
 --local NOT_FOUND = "[ lua ] metadata '%s' was not found in source, applying default %s."
 
 if FORMAT == "docx" then
-end
 
-local function get_meta(mt)
-    meta = util_get_meta(mt, default_meta, meta_key)
-    return meta
-end
-
-local function get_header_styles(align)
-    local aligns_table = {
-        AlignDefault = stringify(meta["header-default"]),
-        AlignLeft = stringify(meta["header-left"]),
-        AlignCenter = stringify(meta["header-center"]),
-        AlignRight = stringify(meta["header-right"]) }
-    --debug(aligns_table[align])
-    return aligns_table[align]
-end
-local function get_body_styles(align)
-    --pretty.dump(meta)
-    local aligns_table = {
-        AlignDefault = stringify(meta["body-default"]),
-        AlignLeft = stringify(meta["body-left"]),
-        AlignCenter = stringify(meta["body-center"]),
-        AlignRight = stringify(meta["body-right"]) }
-    --debug(aligns_table[align])
-    return aligns_table[align]
-end
-local function apply_cell_styles(el)
-    debug("Table found")
-    local aligns = el.aligns
-    local headers = el.headers
-    local rows = el.rows
-    --pretty.dump(aligns)
-    local body_styles = {}
-    local header_styles = {}
-    for i, v in ipairs(aligns) do
-        table.insert(body_styles, get_body_styles(v))
-        table.insert(header_styles, get_header_styles(v))
+    local function get_meta(mt)
+        meta = util_get_meta(mt, default_meta, meta_key)
+        return meta
     end
-    --pretty.dump(body_styles)
-    --pretty.dump(header_styles)
 
-    for i, header in ipairs(headers) do
-        local header_cell = pandoc.Div(header)
-        header_cell["attr"]["attributes"]["custom-style"] = stringify(header_styles[i])
-        el.headers[i] = { header_cell }
-        --pretty.dump(header_cell)
+    local function get_header_styles(align)
+        local aligns_table = {
+            AlignDefault = stringify(meta["header-default"]),
+            AlignLeft = stringify(meta["header-left"]),
+            AlignCenter = stringify(meta["header-center"]),
+            AlignRight = stringify(meta["header-right"]) }
+        --debug(aligns_table[align])
+        return aligns_table[align]
     end
-    --pretty.dump(headers)
-
-    for i, row in ipairs(rows) do
-        for j, cell in ipairs(row) do
-            local body_cell = pandoc.Div(cell)
-            body_cell["attr"]["attributes"]["custom-style"] = stringify(body_styles[j])
-            el.rows[i][j] = { body_cell }
+    local function get_body_styles(align)
+        --pretty.dump(meta)
+        local aligns_table = {
+            AlignDefault = stringify(meta["body-default"]),
+            AlignLeft = stringify(meta["body-left"]),
+            AlignCenter = stringify(meta["body-center"]),
+            AlignRight = stringify(meta["body-right"]) }
+        --debug(aligns_table[align])
+        return aligns_table[align]
+    end
+    local function apply_cell_styles(el)
+        debug("Table found")
+        local aligns = el.aligns
+        local headers = el.headers
+        local rows = el.rows
+        --pretty.dump(aligns)
+        local body_styles = {}
+        local header_styles = {}
+        for i, v in ipairs(aligns) do
+            table.insert(body_styles, get_body_styles(v))
+            table.insert(header_styles, get_header_styles(v))
         end
+        --pretty.dump(body_styles)
+        --pretty.dump(header_styles)
+
+        for i, header in ipairs(headers) do
+            local header_cell = pandoc.Div(header)
+            header_cell["attr"]["attributes"]["custom-style"] = stringify(header_styles[i])
+            el.headers[i] = { header_cell }
+            --pretty.dump(header_cell)
+        end
+        --pretty.dump(headers)
+
+        for i, row in ipairs(rows) do
+            for j, cell in ipairs(row) do
+                local body_cell = pandoc.Div(cell)
+                body_cell["attr"]["attributes"]["custom-style"] = stringify(body_styles[j])
+                el.rows[i][j] = { body_cell }
+            end
+        end
+        --pretty.dump(el)
+        return el
     end
-    --pretty.dump(el)
-    return el
+    return { { Meta = get_meta }, { Table = apply_cell_styles } }
 end
-return { { Meta = get_meta }, { Table = apply_cell_styles } }
