d90d4fb661cf95a7ceeee6e7837ccd0f3752eb1a
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2020-08-16 19:12:02 +0900

    update table-width.lua
    - import pl.List as List
    - remove pandoc version limitation
    - add fill_widths() popped out from table_width()
    - add merge_colspecs()

--- lua/table-width.lua
+++ lua/table-width.lua
@@ -27,6 +27,8 @@ where,
 --local stringify = require("pandoc.utils").stringify
 
 --local pretty = require("pl.pretty")
+--local tablex = require("pl.tablex")
+local List = require("pl.List")
 require("pl.stringx").import()
 
 local debug = require("pandocker.utils").debug
@@ -36,74 +38,91 @@ local MESSAGE = "[ lua ] Div in 'table' class found"
 local NOHEADER_MESSAGE = "[ lua ] Move header row to general rows"
 local WIDTH_MESSAGE = "[ lua ] Adjust column ratio"
 
-if PANDOC_VERSION < { 2, 10 } then
-    local function get_widths(attr)
-        local widths = {}
-        for _, v in ipairs(attr:split(",")) do
-            v = tonumber(v)
-            if v == nil then
-                v = 0
-            end
-            if v == 0 and FORMAT == "docx" then
-                v = 0.01
-            end
-            table.insert(widths, v)
+local function get_widths(attr)
+    local widths = List()
+    for num in string.gmatch(attr, "(%d+%.?%d*),?") do
+        --debug(num)
+        num = tonumber(num)
+        if num == 0 and FORMAT == "docx" then
+            v = 0.01
         end
-        return widths
+        widths:append(num)
     end
-
-    local function sum_content(el)
-        local sum = 0
-        --pretty.dump(el)
-        for i, v in ipairs(el) do
-            sum = sum + #v
-        end
-        return sum
-    end
-
-    local function table_width(el)
-        if el.classes:find("table") then
-            if #el.content == 1 and el.content[1].tag == "Table" then
-                debug(MESSAGE)
-                --pretty.dump(el.attributes["width"])
-                local widths = el.attributes["width"]
-                local noheader = get_tf(el.attributes["noheader"], false)
-                local tbl = el.content[1]
-                local col_max = #tbl.widths
-                if noheader and tbl.headers ~= {} then
-                    debug(NOHEADER_MESSAGE)
-                    if #tbl.rows == 1 and sum_content(tbl.rows[1]) == 0 then
-                        debug("[ lua ] header row overrides first body row")
-                        tbl.rows[1] = tbl.headers
-                    else
-                        debug("[ lua ] header row is inserted at head of body rows")
-                        table.insert(tbl.rows, 1, tbl.headers)
-                    end
-                    tbl.headers = {}
-                end
-                if widths ~= nil then
-                    widths = widths:match("%[(.*)%]")
-                    widths = get_widths(widths)
-                    debug(WIDTH_MESSAGE)
-                else
-                    widths = {}
-                end
-                while col_max > #widths do
-                    if FORMAT == "docx" then
-                        table.insert(widths, 0.01)
-                    else
-                        table.insert(widths, 0)
-                    end
-                end
-
-                --pretty.dump(widths)
-                tbl.widths = widths
-                return tbl
-            end
-        end
-    end
-
-    return { { Div = table_width } }
-else
-    debug("[ lua ] Pandoc version mismatched. This filter works with Pandoc<2.10")
+    return widths
 end
+
+local function sum_content(el)
+    local sum = 0
+    --pretty.dump(el)
+    for i, v in ipairs(el) do
+        sum = sum + #v
+    end
+    return sum
+end
+
+local function fill_widths(col_max, widths)
+    while col_max > #widths do
+        if FORMAT == "docx" then
+            table.insert(widths, 0.01)
+        else
+            table.insert(widths, 0)
+        end
+    end
+    return widths
+end
+
+local function merge_colspecs(colspecs, widths)
+    for idx, _ in ipairs(widths) do
+        table.insert(colspecs[idx], widths[idx])
+    end
+    return colspecs
+end
+
+local function table_width(el)
+    if el.classes:find("table") then
+        if #el.content == 1 and el.content[1].tag == "Table" then
+            debug(MESSAGE)
+            --pretty.dump(el.attributes["width"])
+            local widths = el.attributes["width"]
+            local noheader = get_tf(el.attributes["noheader"], false)
+            local tbl = el.content[1] -- Table
+
+            local col_max = 1
+            if PANDOC_VERSION < { 2, 10 } then
+                col_max = #tbl.widths
+            else
+                col_max = #tbl.colspecs
+            end
+
+            if noheader and tbl.headers ~= {} then
+                debug(NOHEADER_MESSAGE)
+                if #tbl.rows == 1 and sum_content(tbl.rows[1]) == 0 then
+                    debug("[ lua ] header row overrides first body row")
+                    tbl.rows[1] = tbl.headers
+                else
+                    debug("[ lua ] header row is inserted at head of body rows")
+                    table.insert(tbl.rows, 1, tbl.headers)
+                end
+                tbl.headers = {}
+            end
+            if widths ~= nil then
+                widths = get_widths(widths)
+                debug(WIDTH_MESSAGE)
+            else
+                widths = {}
+            end
+            widths = fill_widths(col_max, widths)
+            if PANDOC_VERSION < { 2, 10 } then
+                tbl.widths = widths
+            else
+                tbl.colspecs = merge_colspecs(tbl.colspecs, widths)
+                --pretty.dump(tbl.colspecs)
+            end
+
+            --pretty.dump(widths)
+            return tbl
+        end
+    end
+end
+
+return { { Div = table_width } }
