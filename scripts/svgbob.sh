c066e2c58ada1fb67c6e589fbcbbb4341d8fe862
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2022-05-04 18:28:49 +0900

    update svgbob.sh - remove time command

--- scripts/svgbob.sh
+++ scripts/svgbob.sh
@@ -5,15 +5,15 @@ sudo apt -y install gcc-mingw-w64
 rustup update
 
 rustup target add x86_64-unknown-linux-musl
-time cargo install svgbob_cli --root=build/linux --target=x86_64-unknown-linux-musl
+cargo install svgbob_cli --root=build/linux --target=x86_64-unknown-linux-musl
 mv build/linux/bin/svgbob_cli build/svgbob
 
 rustup target add x86_64-pc-windows-gnu
-time cargo install svgbob_cli --root=build/windows --target=x86_64-pc-windows-gnu
+cargo install svgbob_cli --root=build/windows --target=x86_64-pc-windows-gnu
 mv build/windows/bin/svgbob_cli.exe build/svgbob.exe
 
 rustup target add x86_64-apple-darwin
-time cargo install svgbob_cli --root=build/osx --target=x86_64-apple-darwin
+cargo install svgbob_cli --root=build/osx --target=x86_64-apple-darwin
 mv build/osx/bin/svgbob_cli build/svgbob.bin
 
 ls build
