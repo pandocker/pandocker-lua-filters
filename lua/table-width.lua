40fd4ee070502366702d6fb2a6b7a108618b1052
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2022-05-10 22:12:02 +0900

    update table-width.lua - move core function to `table_width()`

--- lua/table-width.lua
+++ lua/table-width.lua
@@ -128,84 +128,83 @@ local function merge_colspecs(colspecs, widths)
     return colspecs
 end
 
-local function table_width(el, attr)
+local function table_width(tbl, attr)
+    local headers = {}
+    local body = {}
+    debug(MESSAGE)
+    --pretty.dump(el.attributes["width"])
+    local widths = attr["width"]
+    local noheader = get_tf(attr["noheader"], false)
+    local empty_row = { empty_attr, {} }
 
+    local col_max = 1
+    if PANDOC_VERSION < { 2, 10 } then
+        headers = tbl.headers
+        body = tbl.rows
+        col_max = #tbl.widths
+    else
+        headers = tbl.head[2]
+        body = tbl.bodies[1].body
+        --pretty.dump(body)
+        col_max = #tbl.colspecs
+        for _, v in ipairs(tablex.range(1, col_max)) do
+            table.insert(empty_row[2], empty_cell)
+        end
+    end
+
+    if noheader and headers ~= {} then
+        debug(NOHEADER_MESSAGE)
+        if PANDOC_VERSION < { 2, 10 } then
+            if #body == 1 and sum_content(body[1]) == 0 then
+                -- valid header row + first body row is blank
+                -- -> remove header row + first body row has ex-header row contents
+                debug("[ lua ] header row overrides first body row && remove header row")
+                tbl.rows[1] = headers
+            else
+                -- valid header row + first body row is not blank
+                -- -> remove header row + ex-header row stacks at top of body rows
+                debug("[ lua ] header row is inserted at head of body rows")
+                table.insert(tbl.rows, 1, headers)
+            end
+            tbl.headers = {}
+        else
+            --debug(#body)
+            --debug(sum_content(body[1]))
+            -- valid header row + first body row is blank
+            if #body == 1 and sum_content(body[1]) == 0 then
+                debug("[ lua ] header row overrides first body row && remove header row (pandoc >= 2.10)")
+                tbl.bodies[1].body = headers
+            else
+                debug("[ lua ] header row is inserted at head of body rows (pandoc >= 2.10)")
+                --pretty.dump(tbl.bodies[1].body)
+                --pretty.dump(headers)
+                table.insert(tbl.bodies[1].body, 1, headers[1])
+            end
+            tbl.head = { empty_attr, {  } }
+        end
+    end
+    if widths ~= nil then
+        widths = get_widths(widths)
+        debug(WIDTH_MESSAGE)
+    else
+        widths = {}
+    end
+    widths = fill_widths(col_max, widths)
+    if PANDOC_VERSION < { 2, 10 } then
+        tbl.widths = widths
+    else
+        tbl.colspecs = merge_colspecs(tbl.colspecs, widths)
+        --pretty.dump(tbl.colspecs)
+    end
+
+    --pretty.dump(widths)
+    return tbl
 end
 
 local function table_finder(el)
-    local headers = {}
-    local body = {}
     if el.classes:find("table") then
         if #el.content == 1 and el.content[1].tag == "Table" then
-            debug(MESSAGE)
-            --pretty.dump(el.attributes["width"])
-            local widths = el.attributes["width"]
-            local noheader = get_tf(el.attributes["noheader"], false)
-            local tbl = el.content[1] -- Table
-            local empty_row = { empty_attr, {} }
-
-            local col_max = 1
-            if PANDOC_VERSION < { 2, 10 } then
-                headers = tbl.headers
-                body = tbl.rows
-                col_max = #tbl.widths
-            else
-                headers = tbl.head[2]
-                body = tbl.bodies[1].body
-                --pretty.dump(body)
-                col_max = #tbl.colspecs
-                for _, v in ipairs(tablex.range(1, col_max)) do
-                    table.insert(empty_row[2], empty_cell)
-                end
-            end
-
-            if noheader and headers ~= {} then
-                debug(NOHEADER_MESSAGE)
-                if PANDOC_VERSION < { 2, 10 } then
-                    if #body == 1 and sum_content(body[1]) == 0 then
-                        -- valid header row + first body row is blank
-                        -- -> remove header row + first body row has ex-header row contents
-                        debug("[ lua ] header row overrides first body row && remove header row")
-                        tbl.rows[1] = headers
-                    else
-                        -- valid header row + first body row is not blank
-                        -- -> remove header row + ex-header row stacks at top of body rows
-                        debug("[ lua ] header row is inserted at head of body rows")
-                        table.insert(tbl.rows, 1, headers)
-                    end
-                    tbl.headers = {}
-                else
-                    --debug(#body)
-                    --debug(sum_content(body[1]))
-                    -- valid header row + first body row is blank
-                    if #body == 1 and sum_content(body[1]) == 0 then
-                        debug("[ lua ] header row overrides first body row && remove header row (pandoc >= 2.10)")
-                        tbl.bodies[1].body = headers
-                    else
-                        debug("[ lua ] header row is inserted at head of body rows (pandoc >= 2.10)")
-                        --pretty.dump(tbl.bodies[1].body)
-                        --pretty.dump(headers)
-                        table.insert(tbl.bodies[1].body, 1, headers[1])
-                    end
-                    tbl.head = { empty_attr, {  } }
-                end
-            end
-            if widths ~= nil then
-                widths = get_widths(widths)
-                debug(WIDTH_MESSAGE)
-            else
-                widths = {}
-            end
-            widths = fill_widths(col_max, widths)
-            if PANDOC_VERSION < { 2, 10 } then
-                tbl.widths = widths
-            else
-                tbl.colspecs = merge_colspecs(tbl.colspecs, widths)
-                --pretty.dump(tbl.colspecs)
-            end
-
-            --pretty.dump(widths)
-            return tbl
+            table_width(el.content[1], el.attributes)
         end
     end
 end
