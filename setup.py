807c060b1a997c78d7bdd6bd3e7512c24c9fe00b
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2022-06-06 22:29:43 +0900

    update setup.py - wavedrom==2.0.3post2

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["wavedrom>2.0.3*", "aafigure"],  # Optional
+    install_requires=["wavedrom==2.0.3post2", "aafigure"],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
