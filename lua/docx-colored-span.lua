68d6209d2eb1e751c025c3a79dfedc8e4c00e521
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2023-06-12 03:33:19 +0900

    update docx-colored-span.lua - it works when color is a valid RGB value

--- lua/docx-colored-span.lua
+++ lua/docx-colored-span.lua
@@ -2,22 +2,34 @@
 # docx-colored-span.lua
 ]]
 
+local List = require("pandoc").List
+local stringify = require("pandoc.utils").stringify
+
 local debug = require("pandocker.utils").debug
 local MESSAGE = "[ lua ] Colored span found - 0x%s"
+local KEY = "color"
 
-local COLORHEAD = [[<w:color w:val="%s" />]]
+local COLORHEAD = "<w:r><w:rPr><w:color w:val=\"%s\"/></w:rPr>`"
+local COLORFOOT = "</w:r>"
+
+--[[
+`<w:r><w:rPr><w:color w:val="0000FF"/></w:rPr>`{=openxml}
+**Span**
+`</w:r>`{=openxml}
+]]
 
 if FORMAT == "docx" then
-    function Span(el)
-        if el.classes:find "colored" then
+    local function replace(el)
+        if not List({ nil, "" }):includes(el.attributes[KEY]) then
+            -- 'comment' attribute value is not blank nor nil
+            local color_name = el.attributes[KEY]
+            debug(string.format(MESSAGE, tostring(color_name)))
+            local comment_start = pandoc.RawInline("openxml", string.format(COLORHEAD, color_name))
+            local comment_end = pandoc.RawInline("openxml", COLORFOOT)
+            el.attributes[KEY] = nil
 
-            local color = el.attributes["color"] or "000000"
-            debug(string.format(MESSAGE, color))
-
-            table.insert(el.content, pandoc.RawInline("openxml", string.format(COLORHEAD, color)))
-            --table.insert(el.content, pandoc.RawInline("openxml", "</w:t></w:r><!--inserted-->"))
+            return pandoc.Span({ comment_start, el, comment_end })
         end
-        return el
     end
-    return { { Span = Span } }
+    return { { Span = replace } }
 end
